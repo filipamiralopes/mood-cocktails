3a72d770eb888c52c7e010ec9ebc8a13ac0de4f0
Filipa <filipamnmlopes@hotmail.com>
2024-06-07 12:10:05 +0200

    cleanup

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,6 @@
 - HomePage (as well as the logo on the Navbar)
 - Menu (CocktailList) --> Cocktail detail --> Edit and Delete
 - Add New Cocktail
-- Don't know what to drink? (GetRandomCocktail)
 - Favourites / Cocktails drank already
 - About this bar
 */
