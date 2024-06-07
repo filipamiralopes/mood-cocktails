9a263ed643f7d28682827a5dd2d1cae5cee55e87
Farnoush Daliran <farnoushfdf@gmail.com>
2024-06-07 17:44:43 +0200

    update cocktail list

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+        {/* welcome Section */}
+        <section >
+            <h1>Welcome to Our Bar!</h1>
+            <p>Don’t know what to drink?</p>
+            <button>Get Your Random Cocktail</button>
+        </section>
+        <section>
+       
+        </section>
+    </div>
+  )
+}
+
+export default HomePage
