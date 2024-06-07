bf596df03f9b6fba798723604f91b2c5540c6e45
Filipa <filipamnmlopes@hotmail.com>
2024-06-07 14:54:23 +0200

    Cocktail Detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-
 import HomePage from './pages/HomePage'
 import SideBar from './components/SideBar'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import CocktailList from './pages/CocktailList'
+import CocktailDetail from "./pages/CocktailDetail"
 
 function App() {
 
@@ -18,6 +18,7 @@ function App() {
      <Routes>
        <Route path='/' element={<HomePage />} />
        <Route path='/cocktails' element={<CocktailList />} />
+       <Route path="/cocktails/:cocktailId" element={<CocktailDetail/>} />
      </Routes>
 
      <Footer />
--- src/pages/CocktailDetail.jsx
+++ src/pages/CocktailDetail.jsx
@@ -1,1 +1,55 @@
-// Add review and rating ?
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const CocktailDetail = () => {
+  const { cocktailId } = useParams();
+
+  const [cocktails, setCocktails] = useState([]);
+  // const { cocktailId } = props;
+
+  const allCocktails = async () => {
+    try {
+      const { data } = await axios("http://localhost:5005/drinks/");
+      setCocktails(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    allCocktails();
+  }, []);
+
+  const filteredCocktail = cocktails.find((oneCocktail) => {
+    if (oneCocktail.id == cocktailId) {
+      return true;
+    }
+  });
+
+  const getIngredients = (object) => {
+    let ingredients = []
+    for(let key in object) {
+        if (object[key]&& key.startsWith("strIngredient")){
+            ingredients.push(object[key])
+        }
+    }
+    return ingredients
+  };
+
+  console.log(filteredCocktail);
+
+  if (!filteredCocktail) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <div className="cocktail-detail">
+      <img src={filteredCocktail.nameThumb} style={{ width: "400px" }} />
+      <h1>{filteredCocktail.name}</h1>
+      <p>{getIngredients(filteredCocktail).join(" | ")}</p>
+    </div>
+  );
+};
+
+export default CocktailDetail;
