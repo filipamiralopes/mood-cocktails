c4f533d5f219f441ef44af9c7518eb0e06da0ac3
Filipa <filipamnmlopes@hotmail.com>
2024-06-12 10:54:55 +0200

    add link to cocktail detail in your-table

--- src/pages/YourTable.jsx
+++ src/pages/YourTable.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import CocktailCard from "../components/CocktailCard";
+import { Link } from "react-router-dom";
 
 const YourTable = ({ orderedCocktails }) => {
   return (
     <>
       <div className="your-table">
-        {orderedCocktails.length === 0 ? (<h2>No orders yet</h2>) : (<h2>Your orders:</h2>)}
+        {orderedCocktails.length === 0 ? (
+          <h2>No orders yet</h2>
+        ) : (
+          <h2>Your orders:</h2>
+        )}
         {orderedCocktails.map((cocktail) => {
-          return <CocktailCard key={cocktail.id} cocktail={cocktail} />;
+          return (
+            <Link to={`/cocktails/${cocktail.id}`} key={cocktail.id}>
+              <CocktailCard cocktail={cocktail} />
+            </Link>
+          );
         })}
       </div>
     </>
