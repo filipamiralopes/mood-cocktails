f2c048a1fd142a0453892557b60e4fd423e05695
Filipa <filipamnmlopes@hotmail.com>
2024-06-12 11:35:24 +0200

    add instructions/remarks

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -15,20 +15,27 @@ const CocktailCard = ({ cocktail, searchResults }) => {
 
   return (
     <div className="cocktail-card">
-      {searchResults !== undefined ? (
-        <>
-          <img src={cocktail.strDrinkThumb} style={{ width: "300px" }} />
-          <h1>{cocktail.strDrink}</h1>
-        </>
-      ) : (
-        <>
-          <img src={cocktail.image} style={{ width: "300px" }} />
-          <h1>{cocktail.name}</h1>
-        </>
-      )}
-      {(pathname === "/cocktails" || pathname === "/your-table") ? null : (
-        <p>{getIngredients(cocktail).join(" | ")}</p>
-      )}
+      <div>
+        {searchResults !== undefined ? (
+          <>
+            <img src={cocktail.strDrinkThumb} style={{ width: "300px" }} />
+            <h1>{cocktail.strDrink}</h1>
+          </>
+        ) : (
+          <>
+            <img src={cocktail.image} style={{ width: "300px" }} />
+            <h1>{cocktail.name}</h1>
+          </>
+        )}
+        {pathname === "/cocktails" || pathname === "/your-table" ? null : (
+          <p>{getIngredients(cocktail).join(" | ")}</p>
+        )}
+      </div>
+      <div className="cocktail-description">
+        {pathname === "/cocktails" || pathname === "/your-table" ? null : (
+          cocktail.strInstructions ? (<p>{cocktail.strInstructions}</p>) : (<p>{cocktail.remarks}</p>) 
+        )}
+      </div>
     </div>
   );
 };
